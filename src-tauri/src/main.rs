d74a480a0b0d9b0616ac04625eeac50f4918755d
TabulateJarl8 <tabulatejarl8@gmail.com>
2024-07-10 18:43:44 -0400

    communication with rust backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,12 @@
 
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![read_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn read_file(file_path: String) {
+    println!("{}", file_path);
 }
