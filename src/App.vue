89ac4a0cd6ebedde0a193e506c3f5146ad69e153
TabulateJarl8 <tabulatejarl8@gmail.com>
2024-07-10 21:51:38 -0400

    nbt frontend

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-    import {ref} from 'vue';
+    import {ref, watch} from 'vue';
     import FileChooser from './components/FileChooser.vue';
+    import {invoke} from '@tauri-apps/api/tauri'
     const chosenFile = ref < string | null > (null)
+
+    watch(chosenFile, (file) => {
+        invoke('read_file', {filePath: file}).then((message) => console.log(message)).catch((error) => console.error(error))
+    })
 </script>
 
 <template>
