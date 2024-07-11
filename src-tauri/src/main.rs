149eb0355b7001eae2b9ed96ee789fe9a8df52b0
TabulateJarl8 <tabulatejarl8@gmail.com>
2024-07-10 21:50:17 -0400

    basic nbt reading

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::io::Cursor;
+
+use quartz_nbt::io;
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    QuartzIo(#[from] quartz_nbt::io::NbtIoError),
+}
+
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![read_file])
@@ -9,6 +30,12 @@ fn main() {
 }
 
 #[tauri::command]
-fn read_file(file_path: String) {
-    println!("{}", file_path);
+fn read_file(file_path: String) -> Result<(), Error> {
+    let file = std::fs::read(file_path)?;
+
+    let compound = io::read_nbt(&mut Cursor::new(file), io::Flavor::GzCompressed)?;
+
+    println!("{:?}", compound);
+
+    Ok(())
 }
